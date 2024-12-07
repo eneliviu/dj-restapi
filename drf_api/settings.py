b8ecc65f17fdb45522c3ec6aad25656554228a13
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-07 14:36:05 +0000

    Added logout route

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,7 +67,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = 'DEV' in os.environ
 
 
