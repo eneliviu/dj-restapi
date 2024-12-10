72f27532bce1fa0c483e0abd5fdabb37a3c0ebc2
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-10 12:23:33 +0000

    Deployment ready

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -77,8 +77,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
-# DEBUG = 'DEV' in os.environ
+# DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 # To use the API with React app, add environment variables: ALLOWED_HOST and CLIENT_ORIGIN_DEV in heroku
 ALLOWED_HOSTS = [
@@ -199,18 +199,19 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DEV' in os.environ:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if 'DEV' in os.environ:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
 }
-# else:
-#     DATABASES = {
-#          'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#      }
-#     print('connected')
+else:
+    DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     }
+    print('connected')
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net",
