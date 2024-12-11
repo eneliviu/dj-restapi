30b6c64bd3efe60a13e85b6692d09503f8d1a251
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-11 11:31:58 +0000

    FIx settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -46,8 +46,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
-        else 'rest_framework.authentication.TokenAuthentication'
-             # 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'  # 'rest_framework.authentication.TokenAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.PageNumberPagination',
