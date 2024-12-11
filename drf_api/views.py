94115d15136c775ebe00058e6b54c010e076191e
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-11 11:06:51 +0000

    Fix settings

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,20 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
 
-
-class CustomObtainAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({'token': token.key, 'id': token.user_id})
 
 @api_view()
 def root_route(request):
