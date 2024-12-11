b95925c76cf570804543698e18f9751395db0328
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-11 10:10:57 +0000

    FIx settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -55,8 +55,8 @@ REST_FRAMEWORK = {
 
 # if 'DEV' not in os.environ:
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        # 'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 
 REST_USE_JWT = True
