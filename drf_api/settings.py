1bb351fdededc6fcb523df91c5e8efa070980d45
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-11 10:00:43 +0000

    Fix settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -44,7 +44,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        else 'rest_framework.authentication.TokenAuthentication'  # 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        
     )],
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.PageNumberPagination',
@@ -54,8 +55,8 @@ REST_FRAMEWORK = {
 
 # if 'DEV' not in os.environ:
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 
 REST_USE_JWT = True
